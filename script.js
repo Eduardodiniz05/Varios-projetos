5eda22453076d9647a70ef762a6095c63679e6f4
Eduardodiniz05 <eduardo.diniz005@gmail.com>
2021-09-23 17:11:56 -0300

    praticamente terminado o primeiro projeto, adicionado o resto das funcionalidades de uma urna eletronica, como branco e nulo confirmar

--- script.js
+++ script.js
@@ -14,12 +14,16 @@ let numeros = document.querySelector('.d-1-3');
 
 let etapaAtual = 0;
 let numeroAtual ='';
+let votoBranco = false;
+let votos = [];
 
 
 function comecarEtapa () {
     let etapa = etapas[etapaAtual];
     
     let numeroHtml = '';
+    numeroAtual = '';
+    votoBranco = false;
 
     for(let i=0;i<etapa.numero;i++){
         if(i === 0){
@@ -56,10 +60,19 @@ function atualizarInterface() {
 
         let fotosHtml = '';
         for (let i in candidato.fotos) {
-            fotosHtml += `<div class="d-1-image"><img src="/urna/img/${candidato.fotos[i].url}" alt="">${candidato.fotos[i].legenda}</div>`;
+            if (candidato.fotos[i].small){
+            fotosHtml += `<div class="d-1-image small"><img src="/urna/img/${candidato.fotos[i].url}" alt="">${candidato.fotos[i].legenda}</div>`;
+            } else {
+                fotosHtml += `<div class="d-1-image"><img src="/urna/img/${candidato.fotos[i].url}" alt="">${candidato.fotos[i].legenda}</div>`;
+            }
         }
 
         lateral.innerHTML = fotosHtml;
+    } else {
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        descricao.innerHTML = '<div class="aviso--nulo pisca">VOTO NULO</div>'
+
     }
 }
 
@@ -81,15 +94,46 @@ function clicou(n)  {
 }
 
 function branco() {
-    alert('clicou em branco!');
+        votoBranco = true;
+        numeroAtual = ''
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        numeros.innerHTML = '';
+        descricao.innerHTML = '<div class="aviso--branco pisca">VOTO EM BRANCO</div>'
+        lateral.innerHTML = '';
 }
 
 function corrige() {
-    alert('clicou em corrige!');  
+   comecarEtapa(); 
 }
 
 function confirma() {
-    alert('clicou em confirma!');
+    let etapa = etapas[etapaAtual];
+    let votoConfirmado = false;
+
+    if(votoBranco === true) {
+        votoConfirmado = true;
+        votos.push({
+            etapa: etapas[etapaAtual].titulo,
+            voto:'Branco'
+        })
+    } else if(numeroAtual.length === etapa.numero) {
+        votoConfirmado = true;
+        votos.push({
+            etapa: etapas[etapaAtual].titulo,
+            voto: numeroAtual
+        })
+    }
+
+    if (votoConfirmado) {
+        etapaAtual++;
+        if (etapas[etapaAtual] !== undefined) {
+            comecarEtapa();
+        } else {
+            document.querySelector('.tela').innerHTML = '<div class="aviso--grande pisca">FIM</div>'
+            console.log(votos);
+        }
+    }
 }
 
 
